4b125c7e8b4391704b4843927719585fd0298b01
henilpavasiya <henil.pavasiya@gmail.com>
2024-08-08 12:41:43 -0400

    bug fix

--- app/src/main/java/com/example/laundryapp/View/Auth/RegisterActivity.kt
+++ app/src/main/java/com/example/laundryapp/View/Auth/RegisterActivity.kt
@@ -4,15 +4,21 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.example.laundryapp.ViewModel.Auth.RegisterViewModel
 import com.example.laundryapp.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
     lateinit var binding: ActivityRegisterBinding
+    private lateinit var registerViewModel: RegisterViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding=ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        registerViewModel=ViewModelProvider(this)[RegisterViewModel::class.java]
 
         binding.apply {
             buttonRegisterLogin.setOnClickListener{
---
+++ app/src/main/java/com/example/laundryapp/ViewModel/Auth/ForgotPasswordViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.laundryapp.ViewModel.Auth
+
+import androidx.lifecycle.ViewModel
+
+class ForgotPasswordViewModel:ViewModel() {
+
+}
